9e651475be1ed1e7bd90af5a90cab5719e41cbf7
Matawari <102017397+Matawari@users.noreply.github.com>
2023-09-02 00:05:51 +0700

    sa

--- Assets/SoilBarManager.cs
+++ Assets/SoilBarManager.cs
@@ -1,31 +1,66 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class SoilBarManager : MonoBehaviour
 {
-    public Image soilBar;  // The UI Image acting as the soil bar
-    public BagController bagController;  // Reference to the BagController script
-    private Camera _cam;  // Reference to the main camera in the scene
+    public Image soilBar;
+    public BagController bagController;
+    public ParticleSystem smokeEffect;
+    public AudioSource audioSource;
+    public AudioClip smokeSound;
+    private Camera _cam;
+    private bool isDestroyed = false;
 
     private void Start()
     {
-        _cam = Camera.main;  // Get the reference to the main camera
+        _cam = Camera.main;
+        if (smokeEffect != null)
+        {
+            smokeEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            var main = smokeEffect.main;
+            main.stopAction = ParticleSystemStopAction.Callback;
+        }
     }
 
     private void LateUpdate()
     {
-        // Update the fill amount of the soil bar based on the soilAmount in BagController
+        if (isDestroyed) return;
+
+        if (bagController.soilAmount <= 0)
+        {
+            isDestroyed = true;
+
+            if (smokeEffect != null)
+            {
+                smokeEffect.gameObject.SetActive(true);
+                smokeEffect.transform.position = bagController.transform.position;
+                smokeEffect.Play();
+
+                // Deactivate after playing
+                smokeEffect.gameObject.SetActive(false);
+
+                // Reactivate for future use
+                smokeEffect.gameObject.SetActive(true);
+            }
+
+            if (audioSource != null && smokeSound != null)
+            {
+                audioSource.PlayOneShot(smokeSound);
+            }
+
+            Destroy(bagController.gameObject);
+            return;
+        }
+
         float fillAmount = (bagController.soilAmount >= 30) ? 1.0f : bagController.soilAmount / 30.0f;
         soilBar.fillAmount = fillAmount;
 
-        // Update the soilBar's position to follow the bag's position
         Vector3 bagPosition = bagController.transform.position;
-        soilBar.transform.position = bagPosition + new Vector3(0, 0.15f, -0.1f);  // Smaller vertical offset
-
+        soilBar.transform.position = bagPosition + new Vector3(0, 0.15f, -0.1f);
 
         Vector3 toCamera = _cam.transform.position - soilBar.transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(-toCamera, _cam.transform.up);
         soilBar.transform.rotation = lookRotation;
-
     }
 }
